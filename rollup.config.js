605f6c1e454d77923c11e4c021adc07b5b8a54d5
Ali Hassan <alihassan@Alis-MacBook-Air.local>
2019-09-26 22:07:36 +0200

    fix: trying to fix es6 module intilli issue

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,25 @@
 import babel from "rollup-plugin-babel";
 import minify from "rollup-plugin-babel-minify";
+import commonjs from "rollup-plugin-commonjs";
+import resolve from "rollup-plugin-node-resolve";
 module.exports = {
   input: "./src/index.js",
-  output: {
-    file: "dist/index.js",
-    format: "umd",
-    name: "OPStructs",
-    sourcemap: true
-  },
+  output: [
+    {
+      file: "dist/index.js",
+      format: "umd",
+      name: "OPStructs",
+      sourcemap: true
+    },
+    {
+      file: "dist/index.esm.js",
+      format: "es",
+      sourcemap: true
+    }
+  ],
   plugins: [
+    resolve(),
+    commonjs(),
     babel({
       exclude: "node_modules/**"
     }),
